e51ccc0799d8d2bfa87ddb3eb84a069943925c7b
Elohina Guevara <elohina@Elohinas-MacBook-Pro.local>
2018-10-18 23:22:06 +0100

    Add social links

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,37 +1,19 @@
 import React from "react";
 import {
-  FaFacebook,
-  FaGithub,
-  FaGooglePlus,
-  FaInstagram,
-  FaTwitter
+  FaLinkedin,
+  FaGithub
 } from "react-icons/lib/fa";
 // https://gorangajic.github.io/react-icons/fa.html
 
 const SocialLinks = () => (
   <ul className="social">
     <li>
-      <a href="#">
-        <FaFacebook />
+      <a href="https://www.linkedin.com/in/pedrovaldi/" target="_blank" rel="noopener">
+        <FaLinkedin />
       </a>
     </li>
     <li>
-      <a href="#">
-        <FaTwitter />
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <FaGooglePlus />
-      </a>
-    </li>
-    <li>
-      <a href="#">
-        <FaInstagram />
-      </a>
-    </li>
-    <li>
-      <a href="#">
+      <a href="https://github.com/Gringox" target="_blank" rel="noopener">
         <FaGithub />
       </a>
     </li>

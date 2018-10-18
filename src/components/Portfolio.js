17eb5195242ab5cd7dc1450039ccbb88f99b2e8d
Elohina Guevara <elohina@Elohinas-MacBook-Pro.local>
2018-10-18 19:02:14 +0100

    Improve portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { FaPlus, FaTag } from "react-icons/lib/fa";
 
-import Coffee from "../assets/images/portfolio/debiphone.png";
-import Console from "../assets/images/portfolio/roloegol.png";
-import Judah from "../assets/images/portfolio/thejetmanager.jpg";
-import IntoTheLight from "../assets/images/portfolio/wuelto2.png";
-import Farmerboy from "../assets/images/portfolio/bellclick.jpg";
-import Girl from "../assets/images/portfolio/bellbanking2.png";
-import Origami from "../assets/images/portfolio/bellchat.png";
-import Retrocam from "../assets/images/portfolio/prestamos_bellnomina.png";
+import Debiphone from "../assets/images/portfolio/debiphone.png";
+import RoloeGol from "../assets/images/portfolio/roloegol.png";
+import TheJetManager from "../assets/images/portfolio/thejetmanager.jpg";
+import Wuelto from "../assets/images/portfolio/wuelto2.png";
+import Bellbank from "../assets/images/portfolio/bellbanking2.png";
 import Hidromovil from "../assets/images/portfolio/hidromovil.png";
 
 import CoffeeModal from "../assets/images/portfolio/modals/m-coffee.jpg";
@@ -16,173 +13,75 @@ import ConsoleModal from "../assets/images/portfolio/modals/m-console.jpg";
 import JudahModal from "../assets/images/portfolio/modals/m-judah.jpg";
 import IntoTheLightModal from "../assets/images/portfolio/modals/m-intothelight.jpg";
 import FarmerboyModal from "../assets/images/portfolio/modals/m-farmerboy.jpg";
-import GirlModal from "../assets/images/portfolio/modals/m-girl.jpg";
-import OrigamiModal from "../assets/images/portfolio/modals/m-origami.jpg";
 import RetrocamModal from "../assets/images/portfolio/modals/m-retrocam.jpg";
 
 
-const Portfolio = () => (
+const Portfolio = (propos) => {
+  const works = [
+    {
+      name: "Debiphone",
+      key_words: "",
+      description: "",
+      url: "",
+      image: Debiphone
+    },
+    {
+      name: "Debiphone",
+      key_words: "",
+      description: "",
+      url: "",
+      image: TheJetManager
+    },
+    {
+      name: "Debiphone",
+      key_words: "",
+      description: "",
+      url: "",
+      image: Wuelto
+    },
+    {
+      name: "Debiphone",
+      key_words: "",
+      description: "",
+      url: "",
+      image: Bellbank
+    },
+    {
+      name: "Debiphone",
+      key_words: "",
+      description: "",
+      url: "",
+      image: Hidromovil
+    }
+  ];
+  const worksList = works.map((element) => {
+    return (
+      <div className="columns portfolio-item">
+            <div className="item-wrap">
+              <a href="#modal-01" title="">
+                <img alt="" className="item-image" src={element.image} />
+                <div className="overlay">
+                  <div className="portfolio-item-meta">
+                    <h5>{element.name}</h5>
+                    <p>{element.key_words}</p>
+                  </div>
+                </div>
+                <div className="link-icon">
+                  <FaPlus />
+                </div>
+              </a>
+            </div>
+          </div>
+    );
+  })
+
+  return (
   <section id="portfolio">
     <div className="row">
       <div className="twelve columns collapsed">
         <h1>Check Out Some of My Works.</h1>
-
-        <div
-          id="portfolio-wrapper"
-          className="bgrid-quarters s-bgrid-thirds cf"
-        >
-          <div className="columns portfolio-item">
-            <div className="item-wrap">
-              <a href="#modal-01" title="">
-                <img alt="" src={Coffee} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>Coffee</h5>
-                    <p>Illustrration</p>
-                  </div>
-                </div>
-                <div className="link-icon">
-                  <FaPlus />
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div className="columns portfolio-item">
-            <div className="item-wrap">
-              <a href="#modal-02" title="">
-                <img alt="" src={Console} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>Console</h5>
-                    <p>Web Development</p>
-                  </div>
-                </div>
-                <div className="link-icon">
-                  <FaPlus />
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div className="columns portfolio-item">
-            <div className="item-wrap">
-              <a href="#modal-03" title="">
-                <img alt="" src={Judah} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>Judah</h5>
-                    <p>Webdesign</p>
-                  </div>
-                </div>
-                <div className="link-icon">
-                  <FaPlus />
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div className="columns portfolio-item">
-            <div className="item-wrap">
-              <a href="#modal-04" title="">
-                <img alt="" src={IntoTheLight} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>Into The Light</h5>
-                    <p>Photography</p>
-                  </div>
-                </div>
-                <div className="link-icon">
-                  <FaPlus />
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div className="columns portfolio-item">
-            <div className="item-wrap">
-              <a href="#modal-05" title="">
-                <img alt="" src={Farmerboy} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>Farmer Boy</h5>
-                    <p>Branding</p>
-                  </div>
-                </div>
-                <div className="link-icon">
-                  <FaPlus />
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div className="columns portfolio-item">
-            <div className="item-wrap">
-              <a href="#modal-06" title="">
-                <img alt="" src={Girl} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>Girl</h5>
-                    <p>Photography</p>
-                  </div>
-                </div>
-                <div className="link-icon">
-                  <FaPlus />
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div className="columns portfolio-item">
-            <div className="item-wrap">
-              <a href="#modal-07" title="">
-                <img alt="" src={Origami} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>Origami</h5>
-                    <p>Illustrration</p>
-                  </div>
-                </div>
-                <div className="link-icon">
-                  <FaPlus />
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div className="columns portfolio-item">
-            <div className="item-wrap">
-              <a href="#modal-08" title="">
-                <img alt="" src={Retrocam} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>Retrocam</h5>
-                    <p>Web Development</p>
-                  </div>
-                </div>
-                <div className="link-icon">
-                  <FaPlus />
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div className="columns portfolio-item">
-            <div className="item-wrap">
-              <a href="#modal-08" title="">
-                <img alt="" src={Hidromovil} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>Hidromovil</h5>
-                    <p>Web Development</p>
-                  </div>
-                </div>
-                <div className="link-icon">
-                  <FaPlus />
-                </div>
-              </a>
-            </div>
-          </div>
+        <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf">
+          { worksList }
         </div>
       </div>
 
@@ -194,7 +93,7 @@ const Portfolio = () => (
         />
 
         <div className="description-box">
-          <h4>Coffee Cup</h4>
+          <h4>Debiphone Cup</h4>
           <p>
             Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin,
             lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis
@@ -219,7 +118,7 @@ const Portfolio = () => (
         />
 
         <div className="description-box">
-          <h4>Console</h4>
+          <h4>RoloeGol</h4>
           <p>
             Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin,
             lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis
@@ -244,7 +143,7 @@ const Portfolio = () => (
         />
 
         <div className="description-box">
-          <h4>Judah</h4>
+          <h4>TheJetManager</h4>
           <p>
             Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin,
             lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis
@@ -311,56 +210,6 @@ const Portfolio = () => (
         </div>
       </div>
 
-      <div id="modal-06" className="popup-modal mfp-hide">
-        <img
-          className="scale-with-grid"
-          src={GirlModal}
-          alt=""
-        />
-
-        <div className="description-box">
-          <h4>Girl</h4>
-          <p>
-            Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin,
-            lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis
-            sem nibh id elit.
-          </p>
-          <span className="categories">
-            <FaTag />Photography
-          </span>
-        </div>
-
-        <div className="link-box">
-          <a href="http://www.behance.net">Details</a>
-          <a href="#portfolio" className="popup-modal-dismiss">Close</a>
-        </div>
-      </div>
-
-      <div id="modal-07" className="popup-modal mfp-hide">
-        <img
-          className="scale-with-grid"
-          src={OrigamiModal}
-          alt=""
-        />
-
-        <div className="description-box">
-          <h4>Origami</h4>
-          <p>
-            Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin,
-            lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis
-            sem nibh id elit.
-          </p>
-          <span className="categories">
-            <FaTag />Branding, Illustration
-          </span>
-        </div>
-
-        <div className="link-box">
-          <a href="http://www.behance.net">Details</a>
-          <a href="#portfolio" className="popup-modal-dismiss">Close</a>
-        </div>
-      </div>
-
       <div id="modal-08" className="popup-modal mfp-hide">
         <img
           className="scale-with-grid"
@@ -387,6 +236,7 @@ const Portfolio = () => (
       </div>
     </div>
   </section>
-);
+)
+};
 
 export default Portfolio;
